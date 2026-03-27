e156c742cc349a2821a343b80abf1589cb6c9a4b
bainancedj100-tech <bainancedj100-tech@users.noreply.github.com>
2026-03-27 22:20:13 +0100

    fix(firebase): revert storageBucket to .appspot.com to fix 404 error

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyD0i9nDzGoCFc8qm7Z2u9GJ8pk9t1GmZFA",
   authDomain: "tiaret-ride-app.firebaseapp.com",
   projectId: "tiaret-ride-app",
-  storageBucket: "tiaret-ride-app.firebasestorage.app",
+  storageBucket: "tiaret-ride-app.appspot.com",
   messagingSenderId: "774444004936",
   appId: "1:774444004936:web:551d5af910e7413ad68a7d",
   measurementId: "G-375ZT46NT1"
